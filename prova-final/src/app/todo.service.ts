79c209b6455f25d23ab1856b40d16ca397b6fd2c
amanda.piva <amanda.piva01@fatec.sp.gov.br>
2022-12-13 08:40:15 -0300

    criando componente

--- prova-final/src/app/todo.service.ts
+++ prova-final/src/app/todo.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { InterTodo } from './todo';//interface importada do arquivo ts
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+
+  private todoURL = 'https://jsonplaceholder.typicode.com/todos'
 
   constructor() { }
 }
---
+++ prova-final/src/app/todo/todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InterTodo } from '../todo';//interface importada do arquivo ts
+import { UsersService } from './../users.service'; //importando nosso serviço
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+
+}
